afb4885db37f26d9887b840f821e12e437cd8a91
Jmath <jmath0933@gmail.com>
2025-11-12 14:40:28 +0900

    fix: 상세페이지 수정 삭제 추가

---
+++ lib/firebase.ts
@@ -0,0 +1,13 @@
+// lib/firebase.ts
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
